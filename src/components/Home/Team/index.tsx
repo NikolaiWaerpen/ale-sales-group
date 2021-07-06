c26aba585c9bea377dfed6cfcb514ad46868e85a
Nikolai Waerpen <nikolaiwaerpen@gmail.com>
2021-07-06 18:45:33 +0200

    chore: add email & phone to team

--- src/components/Home/Team/index.tsx
+++ src/components/Home/Team/index.tsx
@@ -1,10 +1,12 @@
-/* This example requires Tailwind CSS v2.0+ */
+import { MailIcon, PhoneIcon } from "@heroicons/react/outline";
+
 const people = [
   {
     name: "Pål Jonathan S. Ellingsen",
     role: "Eier / Daglig leder",
     imageUrl: "/images/team/paal.webp",
-    // twitterUrl: "#",
+    phone: "+47 918 20 214",
+    email: "paal@alesalesgroup.no",
     linkedinUrl:
       "https://www.linkedin.com/in/p%C3%A5l-jonathan-sandbu-ellingsen-38769053/",
   },
@@ -12,6 +14,8 @@ const people = [
     name: "Nicolas P. Sørum",
     role: "Partner",
     imageUrl: "/images/team/nicolas.webp",
+    phone: "+47 957 93 452",
+    email: "nic.sorum@gmail.com",
     // twitterUrl: "#",
     linkedinUrl: "https://www.linkedin.com/in/nicolas-p-s%C3%B8rum-3419681aa/",
   },
@@ -53,22 +57,24 @@ export default function Team() {
                       <p className="text-gray-400 font-light">{person.role}</p>
                     </div>
                     <ul className="flex justify-center space-x-5">
-                      {/* <li>
+                      <li>
                         <a
-                          href={person.twitterUrl}
+                          href={`tel:${person.phone}`}
                           className="text-gray-400 hover:text-gray-500"
                         >
-                          <span className="sr-only">Twitter</span>
-                          <svg
-                            className="w-5 h-5"
-                            aria-hidden="true"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                          >
-                            <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
-                          </svg>
+                          <span className="sr-only">Mobil</span>
+                          <PhoneIcon className="h-6 w-6" aria-hidden="true" />
                         </a>
-                      </li> */}
+                      </li>
+                      <li>
+                        <a
+                          href={`mailto:${person.email}?subject=Start en uforpliktende samtale!`}
+                          className="text-gray-400 hover:text-gray-500"
+                        >
+                          <span className="sr-only">Epost</span>
+                          <MailIcon className="h-6 w-6" aria-hidden="true" />
+                        </a>
+                      </li>
                       <li>
                         <a
                           href={person.linkedinUrl}

bfa8d57475f409ddd77de6a75df3947725659ab8
Nikolai Waerpen <nikolaiwaerpen@gmail.com>
2021-07-26 00:36:53 +0200

    feat: add google maps

--- src/components/Home/Map/index.tsx
+++ src/components/Home/Map/index.tsx
@@ -5,11 +5,15 @@ import {
   GoogleMap,
   Marker,
 } from "react-google-maps";
+import { useState } from "react";
+import { useEffect } from "react";
+
+const coordinates = { lat: 59.92805839835617, lng: 10.846220754023395 };
 
 const MyMapComponent = compose(
   withProps({
     googleMapURL:
-      "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyD71ot-ASzM3bNCQPwN-8uiOj8TRaQUN10&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
@@ -17,13 +21,18 @@ const MyMapComponent = compose(
   withScriptjs,
   withGoogleMap
 )((props) => (
-  <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
-    {props.isMarkerShown && (
-      <Marker position={{ lat: -34.397, lng: 150.644 }} />
-    )}
+  <GoogleMap defaultZoom={16.5} defaultCenter={coordinates}>
+    {props.isMarkerShown && <Marker position={coordinates} />}
   </GoogleMap>
 ));
 
 export default function Map() {
-  return <MyMapComponent isMarkerShown />;
+  // const [isMarkerShown, setIsMarkerShown] = useState(false);
+  const [isMarkerShown, setIsMarkerShown] = useState(true);
+
+  // useEffect(() => {
+  //   setTimeout(() => setIsMarkerShown(true), 2000);
+  // }, []);
+
+  return <MyMapComponent isMarkerShown={isMarkerShown} />;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ export default function Home() {
       <Contact />
       <ContactInfo />
       <Team />
-      {/* <Map /> */}
+      <Map />
     </>
   );
 }

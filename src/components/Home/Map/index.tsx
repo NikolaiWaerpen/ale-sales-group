943c183fa9db39f67f375c71959bb65163bed7f5
Nikolai Waerpen <nikolaiwaerpen@gmail.com>
2021-07-26 00:55:30 +0200

    fix: google maps billing

--- src/components/Home/Map/index.tsx
+++ src/components/Home/Map/index.tsx
@@ -8,12 +8,12 @@ import {
 import { useState } from "react";
 import { useEffect } from "react";
 
-const coordinates = { lat: 59.92805839835617, lng: 10.846220754023395 };
+const COORDINATES = { lat: 59.92805839835617, lng: 10.846220754023395 };
+const API_KEY = "AIzaSyCd13m4sCPKcuU3y5CgZR846dFlmp06zhI";
 
 const MyMapComponent = compose(
   withProps({
-    googleMapURL:
-      "https://maps.googleapis.com/maps/api/js?key=AIzaSyD71ot-ASzM3bNCQPwN-8uiOj8TRaQUN10&v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&v=3.exp&libraries=geometry,drawing,places`,
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
@@ -21,8 +21,8 @@ const MyMapComponent = compose(
   withScriptjs,
   withGoogleMap
 )((props) => (
-  <GoogleMap defaultZoom={16.5} defaultCenter={coordinates}>
-    {props.isMarkerShown && <Marker position={coordinates} />}
+  <GoogleMap defaultZoom={16.5} defaultCenter={COORDINATES}>
+    {props.isMarkerShown && <Marker position={COORDINATES} />}
   </GoogleMap>
 ));
 
